0d45319f084c559c302454fceceb4bec2f549ed7
Bilal Bandic <bilal.bandic@systemverification.com>
2021-02-10 11:40:23 +0100

    Create dotnet app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WOODZ4U_Summary_report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
